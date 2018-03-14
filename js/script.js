b886812e750fc2a87e9cda3643c1aa8d9d0f6918
joseimoreno <36063648+joseimoreno@users.noreply.github.com>
2018-03-13 21:23:19 -0700

    lab 11
    finiished

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 /*eslint-env browser*/
 
+function init() {
+    "use strict";
+    var btnLogin = window.document.getElementById("login");
+    btnlogin.addEventListener("click", function () {
+        window.open("login.html", "login", "width=400,height=400");
+    });
+    function showValues(id, value);
+        var newValue = window.documnet.getElementById(id).value = value;
+}
+window.addEventListener("load", init);
